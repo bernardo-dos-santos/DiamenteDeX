94c02368eca143e646162d614d49d5bcc5451e64
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-25 17:21:40 -0300

    Lógica do trabalho

--- DiamenteDeXConsoleApp/Program.cs
+++ DiamenteDeXConsoleApp/Program.cs
@@ -6,14 +6,45 @@
         {
             while(true)
             {
-                Console.WriteLine("Digite um número ímpar");
+                Console.Write("Digite um número ímpar: ");
                 int numero = int.Parse(Console.ReadLine()!);
-
+                Console.WriteLine();
                 while (numero % 2 == 0)
                 {
-                    Console.WriteLine("Comando Inválido, Digite Novamente");
+                    Console.Write("Comando Inválido, Digite Novamente: ");
                     numero = int.Parse(Console.ReadLine()!);
+                    Console.WriteLine();
                 }
+
+                int espacos = numero / 2;
+                int quantosX = 1;
+                for (int i = 0; i <= numero; i++)
+                {
+                    for (int e = 0; e <= espacos; e++ )
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int x = 1; x <= quantosX; x++)
+                    {
+                        Console.Write("X");
+                    }
+                    Console.WriteLine();
+                    if(i < numero / 2)
+                    {
+                        espacos--;
+                        quantosX += 2;
+                    } else
+                    {
+                        espacos++;
+                        quantosX -= 2;                    
+                    }
+                }
+
+
+
+
+
+
 
                 Console.WriteLine("Deseja Continuar (s/n)");
                 string continuar = Console.ReadLine()!.ToUpper();
