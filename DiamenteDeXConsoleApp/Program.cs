312e8a76fbff1fa96ae6a08ef51a010af807c5ed
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-25 16:19:38 -0300

    Criação do loop Principal e tratamento de Entrada

--- DiamenteDeXConsoleApp/Program.cs
+++ DiamenteDeXConsoleApp/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while(true)
+            {
+                Console.WriteLine("Digite um número ímpar");
+                int numero = int.Parse(Console.ReadLine()!);
+
+                while (numero % 2 == 0)
+                {
+                    Console.WriteLine("Comando Inválido, Digite Novamente");
+                    numero = int.Parse(Console.ReadLine()!);
+                }
+
+                Console.WriteLine("Deseja Continuar (s/n)");
+                string continuar = Console.ReadLine()!.ToUpper();
+                if (continuar != "S")
+                    break;
+            }
+            
         }
     }
 }
