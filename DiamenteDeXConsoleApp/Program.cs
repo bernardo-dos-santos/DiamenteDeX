173c593ccce4992ef98f0268c0b78d546ce3d4be
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-26 00:06:18 -0300

    Criação dos métodos DigitarNumero, TratamentoDeEntrada, ExibirDesenho e Continuar

--- DiamenteDeXConsoleApp/Program.cs
+++ DiamenteDeXConsoleApp/Program.cs
@@ -1,57 +1,74 @@
-﻿namespace DiamenteDeXConsoleApp
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DiamenteDeXConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            while(true)
+            bool continuar = true;
+            while (continuar)
             {
-                Console.Write("Digite um número ímpar: ");
-                int numero = int.Parse(Console.ReadLine()!);
-                Console.WriteLine();
-                while (numero % 2 == 0)
-                {
-                    Console.Write("Comando Inválido, Digite Novamente: ");
-                    numero = int.Parse(Console.ReadLine()!);
-                    Console.WriteLine();
-                }
+                
 
-                int espacos = numero / 2;
-                int quantosX = 1;
-                for (int i = 0; i <= numero; i++)
-                {
-                    for (int e = 0; e <= espacos; e++ )
-                    {
-                        Console.Write(" ");
-                    }
-                    for (int x = 1; x <= quantosX; x++)
-                    {
-                        Console.Write("X");
-                    }
-                    Console.WriteLine();
-                    if(i < numero / 2)
-                    {
-                        espacos--;
-                        quantosX += 2;
-                    } else
-                    {
-                        espacos++;
-                        quantosX -= 2;                    
-                    }
-                }
-
-
-
-
-
-
-
-                Console.WriteLine("Deseja Continuar (s/n)");
-                string continuar = Console.ReadLine()!.ToUpper();
-                if (continuar != "S")
-                    break;
             }
-            
+
+
+
+        }
+        static string DigitarNumero()
+        {
+                Console.Write("Digite um número ímpar: ");
+                string numerostring = Console.ReadLine()!;
+                Console.WriteLine();
+                return numerostring;
+        }
+        static int TratamentoDeEntrada(string numerostring)
+        {
+            int numero;
+            while(!int.TryParse(numerostring, out numero) || numero % 2 == 0 )
+            {
+            Console.Write("Comando Inválido, ");
+            numerostring = DigitarNumero();
+            }
+            return numero;
+
+        }
+        static void ExibirDesenho(int numero)
+        {
+            int espacos = numero / 2;
+            int quantosX = 1;
+            for (int i = 0; i <= numero; i++)
+            {
+                for (int e = 0; e <= espacos; e++)
+                {
+                    Console.Write(" ");
+                }
+                for (int x = 1; x <= quantosX; x++)
+                {
+                    Console.Write("X");
+                }
+                Console.WriteLine();
+                if (i < numero / 2)
+                {
+                    espacos--;
+                    quantosX += 2;
+                }
+                else
+                {
+                    espacos++;
+                    quantosX -= 2;
+                }
+            }
+        }
+
+        static bool Continuar()
+        {
+            Console.WriteLine("Deseja Continuar (s/n)");
+            string opcaocontinuar = Console.ReadLine()!.ToUpper();
+            if (opcaocontinuar != "S")
+                return false;
+            return true;
         }
     }
 }
